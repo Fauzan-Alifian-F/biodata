e4b6b047cbd159d03005433bee254fd6361508ad
Fauzan-Alifian-F <161696557+Fauzan-Alifian-F@users.noreply.github.com>
2024-03-20 15:21:18 +0700

    Add files via upload

---
+++ Biodata23.py
@@ -0,0 +1,10 @@
+# Informasi biodata
+name = "Fauzan"
+age = 20
+city = "Majalengka"
+
+# Menampilkan biodata
+print("Biodata:")
+print("Nama:", name)
+print("Umur:", age, "tahun")
+print("Asal Kota:", city)
